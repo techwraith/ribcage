085348fc6d581e66377c5dde36a8d9c23ae3556b
Joey Baker <joey@byjoeybaker.com>
2014-09-30 15:49:10 -0700

    correct the ribcage gen api
    fixes #3

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ var preview = require('ribcage-preview')
   , gen = require('ribcage-gen')
   , docs = require('ribcage-docs')
   , path = require('path')
+  , argv = require('minimist')(process.argv.slice(2))
   , fs = require('fs')
+  , type = argv.t || argv.type || 'backbone'
 
 switch (process.argv[2]) {
   case 'preview':
     preview(path.join(process.cwd(), process.argv[3]))
     break
   case 'gen':
-    gen(process.argv[3], function () {
+    gen({target: argv._[0], type: type}, function () {
       console.log('created')
     })
     break
